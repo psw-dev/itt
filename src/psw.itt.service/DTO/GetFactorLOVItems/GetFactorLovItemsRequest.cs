fc28e61fa52cf2bfce49dd1887c8e24835467f3b
lareb.amir <lareb.amir@psw.gov.pk>
2023-02-03 17:13:40 +0500

    hogia

---
+++ src/psw.itt.service/DTO/GetFactorLOVItems/GetFactorLovItemsRequest.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PSW.ITT.Service.DTO
+{
+    public class GetFactorLovItemsRequest
+    {
+        [JsonPropertyName("agencyId")]
+        public int AgencyId { set; get; }
+
+        [JsonPropertyName("documentTypeCode")]
+        public string DocumentTypeCode { set; get; }
+
+        [JsonPropertyName("tradeTranTypeId")]
+        public int TradeTranTypeID { set; get; }
+
+        [JsonPropertyName("factorList")]
+        public List<FactorInfo> FactorList { get; set; }
+
+        [JsonPropertyName("hsCodeExt")]
+        public string HSCodeExt { set; get; }
+    }
+
+    public class FactorInfo
+    {
+        [JsonPropertyName("factorId")]
+        public int FactorId { set; get; }
+
+        [JsonPropertyName("LOVTable")]
+        public string LOVTableName { get; set; }
+
+        [JsonPropertyName("LOVColumn")]
+        public string LOVColumnName { get; set; }
+    }
+}
