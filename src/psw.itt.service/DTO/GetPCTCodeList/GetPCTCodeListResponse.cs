f6322e95203efa0c7474c0d40f2a864d87017bbe
lareb.amir <lareb.amir@psw.gov.pk>
2023-02-06 13:08:10 +0500

    initial commit

---
+++ src/psw.itt.service/DTO/GetPCTCodeList/GetPCTCodeListResponse.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using PSW.ITT.Data.DTO;
+
+namespace PSW.ITT.Service.DTO
+{
+    public class GetPCTCodeListResponse
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("pctCodeList")]
+        public List<ProductDetail> PctCodeList { get; set; }
+    }
+    
+    public class ProductDetail
+    {
+        [JsonPropertyName("productCode")]
+        public string ProductCode { get; set; }
+
+        [JsonPropertyName("itemDescription")]
+        public string ItemDescription { get; set; }
+
+    }
+}
