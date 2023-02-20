16dc6cc41f2de82c2c7beede1ba907a753c14d75
lareb.amir <lareb.amir@psw.gov.pk>
2023-02-20 13:51:05 +0500

    complete PSW-46898

--- src/psw.itt.data/IRepositories/IProductCodeAgencyLinkRepository.cs
+++ src/psw.itt.data/IRepositories/IProductCodeAgencyLinkRepository.cs
@@ -11,5 +11,9 @@ namespace PSW.ITT.Data.IRepositories
         List<ViewRegulatedHsCodeExt> GetHsCodeExtList(int agencyId, string chapter);
         List<ViewRegulatedHsCodeExt> GetHsCodeExtList(int agencyId);
         List<ViewRegulatedHsCodeExt> GetHsCodeExtList();
+        List<ViewRegulatedHsCode> GetRegulatedHsCodeList(int agencyId, int tradeTranTypeId);
+        List<ViewRegulatedHsCode> GetRegulatedHsCodeList(int agencyId);
+        List<ViewRegulatedHsCode> GetRegulatedHsCodeList();
+        List<HscodeDetails> GetRegulatedHsCodeList(int tradeTranTypeId, int agencyId,  string hsCode);
     }
 }
